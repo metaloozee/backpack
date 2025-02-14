7a7ddadc479bab77e89372030febbc7e702d2169
metaloozee <ayanparkar123@gmail.com>
2025-02-14 14:10:01 +0530

    feat: saved chats

---
+++ src/app/(chat)/c/[id]/page.tsx
@@ -0,0 +1,32 @@
+import { convertToUIMessages } from '@/lib/ai/convertToUIMessages';
+import { checkAuth, getUserAuth } from '@/lib/auth/utils';
+import { db } from '@/lib/db';
+import { Chat, chats } from '@/lib/db/schema/app';
+import { CoreMessage } from 'ai';
+import { and, eq } from 'drizzle-orm';
+import { Chat as PreviewChat } from '@/components/Chat';
+import { notFound } from 'next/navigation';
+
+export default async function ChatPage({ params }: { params: { id: string } }) {
+    await checkAuth();
+    const { session } = await getUserAuth();
+
+    const user = session!.user;
+    const { id: chatId } = await params;
+
+    const [chatData] = await db
+        .select()
+        .from(chats)
+        .where(and(eq(chats.id, chatId), eq(chats.userId, user.id)));
+
+    if (!chatData) {
+        return notFound();
+    }
+
+    const chat: Chat = {
+        ...chatData,
+        messages: convertToUIMessages(chatData.messages as Array<CoreMessage>),
+    };
+
+    return <PreviewChat id={chatId} savedMessages={chat.messages} />;
+}
